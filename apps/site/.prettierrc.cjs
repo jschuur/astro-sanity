784c09eb5156a59ea8f8eed7ff07050f7298fdd8
Joost Schuur <jschuur@jschuur.com>
2023-08-26 16:02:16 +0800

    TypeScript/Prettier setup fixes

---
+++ apps/site/.prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
